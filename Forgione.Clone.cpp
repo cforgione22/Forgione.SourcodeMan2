2a1affb5f66de26eb9044cf882585c42d90643b8
cforgione22 <crystal.forgione22@gmail.com>
2024-07-15 01:59:09 +0000

    added parentheses to the equation and changed the last n2 to n3.

---
+++ Forgione.Clone.cpp
@@ -0,0 +1,32 @@
+//Crystal Forgione
+//24.SU.CIS.1202.201 
+//07-14-24
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+float average(float i1, float i2, float i3)
+{
+  //added perenthesis
+	return (i1 + i2 + i3 ) / 3.0;
+}
+
+int main()
+{
+	float n1 = 0 , n2 = 0, n3 = 0;
+  
+	cout << "This program calculates the average of three numbers." << endl;
+	cout << "First number: "; 
+  cin >> n1;
+	cout << "Second number: ";
+  cin >> n2;
+	cout << "Third number: "; 
+  cin >> n3;
+
+	float a = average(n1, n2, n3);
+
+	cout << setprecision(1) << fixed << "The average is " << a << endl;
+
+	return 0;
+}
